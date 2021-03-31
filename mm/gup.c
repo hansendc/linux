9b5e995bdb07772cc25272da592076a7b12a28c4
Yang Shi <yang.shi@linux.alibaba.com>
2021-03-31 15:17:32 -0700

    mm/migrate: make migrate_pages() return nr_succeeded
    From: Yang Shi <yang.shi@linux.alibaba.com>
    
    The migrate_pages() returns the number of pages that were not migrated,
    or an error code.  When returning an error code, there is no way to know
    how many pages were migrated or not migrated.
    
    In the following patch, migrate_pages() is used to demote pages to PMEM
    node, we need account how many pages are reclaimed (demoted) since page
    reclaim behavior depends on this.  Add *nr_succeeded parameter to make
    migrate_pages() return how many pages are demoted successfully for all
    cases.
    
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: osalvador <osalvador@suse.de>
    
    --
    
    Note: Yang Shi originally wrote the patch, thus the SoB.  There was
    also a Reviewed-by provided since there were some modifications made
    to this after the original work.
    
    Changes since 20210302:
     * Fix definition of CONFIG_MIGRATION=n stub migrate_pages().  Its
       parameters were wrong, but oddly enough did not generate any
       compile errors.
    
    Changes since 20200122:
     * Fix migrate_pages() to manipulate nr_succeeded *value*
       rather than the pointer.

--- mm/gup.c
+++ mm/gup.c
@@ -1550,6 +1550,7 @@ static long check_and_migrate_cma_pages(struct mm_struct *mm,
 	unsigned long i;
 	unsigned long step;
 	bool drain_allow = true;
+	unsigned int nr_succeeded = 0;
 	bool migrate_allow = true;
 	LIST_HEAD(cma_page_list);
 	long ret = nr_pages;
@@ -1606,7 +1607,7 @@ check_again:
 				put_page(pages[i]);
 
 		if (migrate_pages(&cma_page_list, alloc_migration_target, NULL,
-			(unsigned long)&mtc, MIGRATE_SYNC, MR_CONTIG_RANGE)) {
+			(unsigned long)&mtc, MIGRATE_SYNC, MR_CONTIG_RANGE, &nr_succeeded)) {
 			/*
 			 * some of the pages failed migration. Do get_user_pages
 			 * without migration.
